3c316df4aa7e70f21b2aaa1705de15d195e7bad1
saikakde <saikakde121@gmail.com>
2023-04-18 20:09:35 +0530

    init modified

--- run.py
+++ run.py
@@ -1,13 +1,14 @@
-from sis import create_app
+# from sis import create_app
 from dotenv import load_dotenv
 # from os import getenv
 import os
-
-app = create_app()
+from sis import app 
+# app = create_app()
 
 load_dotenv('.env')
 # app.secret_key = os.environ.get('SECRET_KEY')
-app.secret_key = os.getenv('SECRET_KEY')
+
+# app.secret_key = os.getenv('SECRET_KEY')
 
 if __name__ == '__main__':
     app.run(debug = True)
