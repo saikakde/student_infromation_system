ed5c012cca884135c7b19c5168f9d6ddf2472e87
saikakde <saikakde121@gmail.com>
2023-04-18 18:12:13 +0530

    getenv

--- run.py
+++ run.py
@@ -1,11 +1,14 @@
 from sis import create_app
 from dotenv import load_dotenv
-from os import getenv
+# from os import getenv
+import os
 
 app = create_app()
 
 load_dotenv('.env')
-app.secret_key = os.environ.get('SECRET_KEY')
+# app.secret_key = os.environ.get('SECRET_KEY')
+app.secret_key = os.getenv('SECRET_KEY')
+
 if __name__ == '__main__':
     app.run()
     
